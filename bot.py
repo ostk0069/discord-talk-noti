e08b164697d5fa0457ab7e5cc8b792430fee15f5
Takuma Osada <judgment9ttk069@gmail.com>
2020-07-25 20:49:53 +0900

    fix

--- bot.py
+++ bot.py
@@ -8,7 +8,6 @@ dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
 TOKEN = os.environ.get("DISCORD_TOKEN")
-CHANNEL_ID = os.environ.get("CHANNEL_ID")
 
 client = discord.Client()
 
