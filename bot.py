1e570375f886b84609a4eda0e9690d5c8499f7ff
Takuma Osada <judgment9ttk069@gmail.com>
2020-07-25 20:35:28 +0900

    add feature

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import discord
+from datetime import datetime, timedelta
 import os
 from os.path import join, dirname
 from dotenv import load_dotenv
@@ -13,14 +14,32 @@ client = discord.Client()
 #BOTが起動したとき
 @client.event
 async def on_ready():
-    print('起動しました！(\'◇\')ゞ')
+    print('ログインしました')
+    print(client.user.name)  # ボットの名前
+    print(client.user.id)  # ボットのID
+    print(discord.__version__)  # discord.pyのバージョン
+    print('------')
 
 @client.event
 async def on_message(message):
     # Botだったらは無視
     if message.author.bot:
         return
+    if message.content == "/version"
+        await message.channel.send('1.0.0')
     if message.content == '/hello':
         await message.channel.send('hello')
+
+@client.event
+async def on_voice_state_update(member, before, after):
+    if member.guild.id == 監視するサーバーid and (before.channel != after.channel):
+        now = datetime.utcnow() + timedelta(hours=9)
+        alert_channel = client.get_channel(通知させたいテキストチャンネルid)
+        if before.channel is None: 
+            msg = f'{now:%m/%d-%H:%M} に {member.name} が {after.channel.name} に参加したよ'
+            await alert_channel.send(msg)
+        elif after.channel is None: 
+            msg = f'{now:%m/%d-%H:%M} に {member.name} が {before.channel.name} から退出したよ'
+            await alert_channel.send(msg)
 
 client.run(TOKEN)
