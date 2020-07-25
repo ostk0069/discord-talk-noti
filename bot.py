3d30eb6966d22bac762831d1da122ac0e4730291
Takuma Osada <judgment9ttk069@gmail.com>
2020-07-25 17:45:19 +0900

    fix typo

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
-TOKEN = os.environ.get("DISCORD_DISCORDTOKEN")
+TOKEN = os.environ.get("DISCORD_TOKEN")
 
 client = discord.Client()
 
